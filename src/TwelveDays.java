211d196be31362e0a07effa241134959e552a05a
OdonCahierre <windward_student@school.windwardschool.org>
2016-09-07 09:38:58 -0700

    Christmas

---
+++ src/TwelveDays.java
@@ -0,0 +1,21 @@
+
+public class TwelveDays {
+public static void TrueLove(){
+	System.out.println("my true love sent to me:");
+
+	
+}
+public static void One(){
+	System.out.println("and a Partridge in a Pear Tree");
+	
+}
+public static void Two(){
+	System.out.println("Two Turtle Doves");
+	One();
+}
+public static void Three()
+{
+	System.out.println("Three French Hens");
+	Two();
+	}
+}
