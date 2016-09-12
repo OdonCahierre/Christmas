b4b0cd555f162233d3fcbd19e196ee1fa2ba1827
OdonCahierre <ocahierre18@windwardschool.org>
2016-09-12 08:52:32 -0700

    no errors

--- src/TwelveDays.java
+++ src/TwelveDays.java
@@ -1,96 +1,96 @@
-
 public class TwelveDays {
-public static void TrueLove(){
-	System.out.println("my true love sent to me:");
+		public static void TrueLove(){
+			System.out.println("my true love sent to me:");
 
-	
-}
-public static void One(){
-	System.out.println("and a Partridge in a Pear Tree");
-	
-}
-public static void Two(){
-	System.out.println("Two Turtle Doves");
-	One();
-}
-public static void Three()
-{
-	System.out.println("Three French Hens");
-	Two();
-	}
-public static void Four(){
-	System.out.println("Four Callingg Birds")
-	Three();
-}
-public static void Five(){
-	System.out.println("Five Golden Rings");
-	Four();
-}
-public static void Six(){
-		System.out.println("Six Geese a Laying");
-		Five();
-}
-public static void Seven(){
-		System.out.println("Seven Swans a Swimming");
-		Six();
-}
-public static void Eight(){
-		System.out.println("Eight Maids a Milking");
-		Seven();
-}
-public static void Nine(){
-		System.out.println("Nine Ladies Dancing");
-		Eight();
-}
-public static void Ten(){
-		System.out.println("Ten Lords a Leaping");
-		Nine();
-}
-public static void Eleven(){
-		System.out.println("Eleven Pipers Piping");
-		Ten();
-}
-public static void Twelve(){
-		System.out.println("12 Drummers Drumming");
-		Eleven();
-}
-public static void main(String[] args){
-		System.out.println("On the first day of Christmas");
-		TrueLove();
-		System.out.println("A Partridge in a Pear Tree");
-		System.out.println("On the second day of Christmas");
-		TrueLove();
-		Two();
-		System.out.println("On the third day of Christmas");
-		TrueLove();
-		Three();
-		System.out.println("On the fourth day of Christmas");
-		TrueLove();
-		Four();
-		System.out.println("On the fifth day of Christmas");
-		TrueLove();
-		Five();
-		System.out.println("On the sixth day of Christmas");
-		TrueLove();
-		Six();
-		System.out.println("On the seventh day of Christmas");
-		TrueLove();
-		Seven();
-		System.out.println("On the eighth day of Christmas");
-		TrueLove();
-		Eight();
-		System.out.println("On the ninth day of Christmas");
-		TrueLove();
-		Nine();
-		System.out.println("On the tenth day of Christmas");
-		TrueLove();
-		Ten();
-		System.out.println("On the eleventh day of Christmas");
-		TrueLove();
-		Eleven();
-		System.out.println("On the twelfth day of Christmas");
-		TrueLove();
-		Twelve();
-}
+			
+		}
+		public static void One(){
+			System.out.println("and a Partridge in a Pear Tree");
+			
+		}
+		public static void Two(){
+			System.out.println("Two Turtle Doves");
+			One();
+		}
+		public static void Three()
+		{
+			System.out.println("Three French Hens");
+			Two();
+			}
+		public static void Four(){
+			System.out.println("Four Calling Birds");
+			Three();
+		}
+		public static void Five(){
+			System.out.println("Five Golden Rings");
+			Four();
+		}
+		public static void Six(){
+				System.out.println("Six Geese a Laying");
+				Five();
+		}
+		public static void Seven(){
+				System.out.println("Seven Swans a Swimming");
+				Six();
+		}
+		public static void Eight(){
+				System.out.println("Eight Maids a Milking");
+				Seven();
+		}
+		public static void Nine(){
+				System.out.println("Nine Ladies Dancing");
+				Eight();
+		}
+		public static void Ten(){
+				System.out.println("Ten Lords a Leaping");
+				Nine();
+		}
+		public static void Eleven(){
+				System.out.println("Eleven Pipers Piping");
+				Ten();
+		}
+		public static void Twelve(){
+				System.out.println("12 Drummers Drumming");
+				Eleven();
+		}
+		public static void main(String[] args){
+				System.out.println("On the first day of Christmas");
+				TrueLove();
+				System.out.println("A Partridge in a Pear Tree");
+				System.out.println("On the second day of Christmas");
+				TrueLove();
+				Two();
+				System.out.println("On the third day of Christmas");
+				TrueLove();
+				Three();
+				System.out.println("On the fourth day of Christmas");
+				TrueLove();
+				Four();
+				System.out.println("On the fifth day of Christmas");
+				TrueLove();
+				Five();
+				System.out.println("On the sixth day of Christmas");
+				TrueLove();
+				Six();
+				System.out.println("On the seventh day of Christmas");
+				TrueLove();
+				Seven();
+				System.out.println("On the eighth day of Christmas");
+				TrueLove();
+				Eight();
+				System.out.println("On the ninth day of Christmas");
+				TrueLove();
+				Nine();
+				System.out.println("On the tenth day of Christmas");
+				TrueLove();
+				Ten();
+				System.out.println("On the eleventh day of Christmas");
+				TrueLove();
+				Eleven();
+				System.out.println("On the twelfth day of Christmas");
+				TrueLove();
+				Twelve();
+		}
 
-}
+		}
+
